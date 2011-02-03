051489ef7109ed2fb81934cc1ad6c5aff48ab286
miyoko <miyoko@maio.me>
2011-02-03 10:38:07 -0600

    changed readme to markdown, worked on modules/phpcgi.sh, fixed permissions

---
+++ README.md
@@ -0,0 +1,15 @@
+# bashhttpd:
+## an httpd written in bash
+*(its a tad pointless >_<)*
+
+### Developers:
+#### miyoko
+#### ttwj
+#### Hexane
+
+## Roadmap
+* Support basic authentication
+* Support SSL (?)
+* Support logging of users
+* Make it compatible with PHP CGI.
+* Write a CGI system in bash.
--- modules/phpcgi.sh
+++ modules/phpcgi.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-#export the variables to environment variables
-#...
-#call up the php thingy
-#..
-# o_O
-# sucess!
+# opens a phpcgi parser, explosions? i hope not.
+
+# THIS IS FOR CHECKING FOR SCRIPT IMPORT, BECAUSE THIS CAN CAUSE PAIN.
+PHP_CGIPATH=$(which php-cgi)
+if test "$PHP_CGIPATH" == "" ; then
+	echo "php-cgi is required by the bashhttpd cgi module. install the php5-cgi package from your local package manager"
+	break
+	exit 1
+fi
+
+function phpcgi.Parse () {
+	#phpcgi.Parse <file>
+}
